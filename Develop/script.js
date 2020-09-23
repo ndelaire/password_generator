1af734fb0dc030bf70f40be5c4c565548bfe0257
Nadine Delaire <ndelaire16@gmail.com>
2020-09-22 17:39:41 -0700

    updating arrays

--- Develop/script.js
+++ Develop/script.js
@@ -13,7 +13,7 @@
 var lowercaseArr = ["a","b", "c","d", "e", "f", "g", "h", "i", "j", "m","l", "n", "o", "p", "q", "r", "s", "t","u", "v","w","x", "y,", "z"]
 var uppercaseArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var numbersArr = [0,1,2,3,4,5,6,7,8,9]
-var specialCharArr = ["@", "!", "%", "^"]
+var specialCharArr = ["@", "!", "%", "^", "#", "&", "*", "(", ")", "?", "!"]
 // Code I started, trying something else and will come back 
 
 function generatePassword() {
